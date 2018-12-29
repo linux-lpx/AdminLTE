85dd9f5611e5114b1fed1dc2936dcd2d1ecf0cdf
林平星-工作 <1358977140@qq.com>
2018-12-29 10:22:38 +0800

    新增一个存放复用率高的常量的js文件，内容见文件

---
+++ pages/tables/constant.js
@@ -0,0 +1,14 @@
+// 常量都放在这个js文件中，便于维护
+
+var ip_loc = 'http://localhost:';
+var ip_txy = 'http://118.25.107.131:';
+var port = '8081';
+function getIpLocal() {
+    return ip_loc;
+}
+function getIpTxy() {
+    return ip_txy;
+}
+function getPort() {
+    return port;
+}
