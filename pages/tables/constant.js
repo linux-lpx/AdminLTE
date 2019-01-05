a96fbac30829f61625687a305d53b22bb682c686
林平星 <1358977140@qq.com>
2019-01-05 19:08:27 +0800

    修改了js缩进（4格 --> 2格）

--- pages/tables/constant.js
+++ pages/tables/constant.js
@@ -3,12 +3,15 @@
 var ip_loc = 'http://localhost:';
 var ip_txy = 'http://118.25.107.131:';
 var port = '8081';
+
 function getIpLocal() {
-    return ip_loc;
+  return ip_loc;
 }
+
 function getIpTxy() {
-    return ip_txy;
+  return ip_txy;
 }
+
 function getPort() {
-    return port;
+  return port;
 }
