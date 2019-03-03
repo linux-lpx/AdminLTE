02386841155a2c78f8db88fa1d503550cd04ab69
林平星 <1358977140@qq.com>
2019-03-03 17:58:28 +0800

    添加网点调拨历史记录页面 完善代码

--- dist/js/pages/bvms-system.js
+++ dist/js/pages/bvms-system.js
@@ -37,5 +37,93 @@
         break;
     }
     return currencyDesc;
+  };
+  window.util.showProcess = function (type, process) {
+    let processDesc;
+    switch (type) {
+      case 1:
+        switch (process) {
+          case 1:
+            processDesc = '已申请';
+            break;
+          case 2:
+            processDesc = '已授权';
+            break;
+          case 3:
+            processDesc = '授权拒绝';
+            break;
+          case 4:
+            processDesc = '已审核';
+            break;
+          case 5:
+            processDesc = '审核拒绝';
+            break;
+          case 6:
+            processDesc = '网点 => 在途';
+            break;
+          case 7:
+            processDesc = '';
+            break;
+          case 8:
+            processDesc = '在途 => 金库';
+            break;
+          default:
+            processDesc = '';
+            break;
+        }
+        break;
+      case 2:
+        switch (process) {
+          case 1:
+            processDesc = '已申请';
+            break;
+          case 2:
+            processDesc = '已授权';
+            break;
+          case 3:
+            processDesc = '授权拒绝';
+            break;
+          case 4:
+            processDesc = '已审核';
+            break;
+          case 5:
+            processDesc = '审核拒绝';
+            break;
+          case 6:
+            processDesc = '已装箱';
+            break;
+          case 7:
+            processDesc = '';
+            break;
+          case 8:
+            processDesc = '已装箱确认';
+            break;
+          case 9:
+            processDesc = '装箱确认拒绝';
+            break;
+          case 10:
+            processDesc = '已出库确认';
+            break;
+          case 11:
+            processDesc = '出库确认拒绝';
+            break;
+          case 12:
+            processDesc = '目标机构已出库';
+            break;
+          case 13:
+            processDesc = '';
+            break;
+          case 14:
+            processDesc = '申请机构已经入库';
+            break;
+          default:
+            processDesc = '';
+            break;
+        }
+        break;
+      default:
+        break;
+    }
+    return processDesc;
   }
 })(window);
